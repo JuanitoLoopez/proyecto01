54d967472e950e5ca905fb19df50ac16636e6cd2
JuanitoLopez <66218285+JuanitoLoopez@users.noreply.github.com>
2023-03-03 05:05:41 +0000

    Second

--- archivos_base/Solver.java
+++ archivos_base/Solver.java
@@ -4,92 +4,222 @@
     pero es libre de crear otros metodos y clases en este u otro archivo que desee.
 */
 
-public class Solver{
+public class Solver {
 
-    public Solver(){
-        //Sientase libre de implementar el contructor de la forma que usted lo desee
-        
+    private boolean isExit = false;
+    private Node node;
+
+    public Solver() {
+        // Sientase libre de implementar el contructor de la forma que usted lo desee
+
     }
 
-    public String solve(Maze maze){
-        //Implemente su metodo aqui. Sientase libre de implementar métodos adicionales
+    public String solve(Maze maze) {
+        // Implemente su metodo aqui. Sientase libre de implementar métodos adicionales
 
         String result = "[";
-        boolean isExit = false;
-        
-        while(!isExit) {
-            
-            Node node = maze.getStartingSpace();
-            System.out.println("node: " + "x "+ node.xIndex + ", y " + node.yIndex + ", z " + node.zIndex);
-            
-            // ALGORITMO DE BUSQUEDA startNode => actualNode
-            // result += moveToSouth(maze, node);
+        isExit = false;
 
-            if(node.east) {
-                node = maze.moveEast(node);
-                System.out.println("node: " + "x "+ node.xIndex + ", y " + node.yIndex + ", z " + node.zIndex);
-                if(maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
-                    Node.resetNodesCount();
-                    result += "E";
-                    return result + "]";
-                } else {
-                    result += "E,";
-                }
-            }
+        while (!isExit) {
+            // System.out.println("node: " + "x "+ node.xIndex + ", y " + node.yIndex + ", z
+            // " + node.zIndex);
 
-            if(node.south) {
-                node = maze.moveSouth(node);
-                System.out.println("node: " + "x "+ node.xIndex + ", y " + node.yIndex + ", z " + node.zIndex);
-                if(maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
-                    Node.resetNodesCount();
-                    result += "S";
-                    return result + "]";
-                } else {
-                    result += "S,";
-                }
-            }
+            node = maze.getStartingSpace();
 
-            if(node.up) {
-                node = maze.moveUp(node); // U
-                System.out.println("node: " + "x "+ node.xIndex + ", y " + node.yIndex + ", z " + node.zIndex);
-                if(maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
-                    Node.resetNodesCount();
-                    result += "U";
-                    return result + "]";
-                } else {
-                    result += "U,";
-                }
-            }
+            if (!isExit)
+                result += moveToEast(maze);
+            if (!isExit)
+                result += moveToSouth(maze);
+            if (!isExit)
+                result += moveToUp(maze);
+            if (!isExit)
+                result += moveToNorth(maze);
+            if (!isExit)
+                result += moveToWest(maze);
+            if (!isExit)
+                result += moveToDown(maze);
 
-            if(node.north) {
-                node = maze.moveNorth(node);
-                System.out.println("node: " + "x "+ node.xIndex + ", y " + node.yIndex + ", z " + node.zIndex);
-                if(maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
-                    Node.resetNodesCount();
-                    result += "N";
-                    return result + "]";
-                } else {
-                    result += "N,";
-                }
-            }
+            // if (node.east) {
+            // node = maze.moveEast(node);
+            // if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+            // Node.resetNodesCount();
+            // isExit = true;
+            // return result + "E]";
+            // } else {
+            // result += "E,";
+            // }
+            // }
 
+            // if (node.south) {
+            // node = maze.moveSouth(node);
+            // if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+            // Node.resetNodesCount();
+            // isExit = true;
+            // return result + "S]";
+            // } else {
+            // result += "S,";
+            // }
+            // }
 
-            // maze.moveNorth(actualNode); // N
-            // maze.moveWest(actualNode); // W
-            // maze.moveDown(actualNode); // D           
+            // if (node.up) {
+            // node = maze.moveUp(node); // U
+            // if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+            // Node.resetNodesCount();
+            // isExit = true;
+            // return result + "U]";
+            // } else {
+            // result += "U,";
+            // }
+            // }
 
-            isExit = true;
+            // if (node.north) {
+            // node = maze.moveNorth(node);
+            // if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+            // Node.resetNodesCount();
+            // isExit = true;
+            // return result + "N]";
+            // } else {
+            // result += "N,";
+            // }
+            // }
 
+            // if (node.west) {
+            // node = maze.moveWest(node);
+            // if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+            // Node.resetNodesCount();
+            // isExit = true;
+            // return result + "W]";
+            // } else {
+            // result += "W,";
+            // }
+            // }
+
+            // if (node.down) {
+            // node = maze.moveDown(node);
+            // if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+            // Node.resetNodesCount();
+            // isExit = true;
+            // return result + "D]";
+            // } else {
+            // result += "D,";
+            // }
+            // }
+
+            // isExit = true;
         }
 
         return result + "]";
     }
 
-    String moveToSouth(Maze maze, Node actualNode) {
-        if(actualNode.south) {
-            maze.moveSouth(actualNode);
+    private String moveToEast(Maze maze) {
+        if (node.east) {
+            System.out.println("SI FUNCIONA ==> EAST");
+            node = maze.moveEast(node);
+
+            if (node.danger)
+                return "";
+
+            if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+                isExit = true;
+                Node.resetNodesCount();
+                return "E";
+            } else {
+                return "E,";
+            }
         }
-        return "S";
+        return "";
+    }
+
+    private String moveToSouth(Maze maze) {
+        if (node.south) {
+            System.out.println("SI FUNCIONA ==> SOUTH");
+            node = maze.moveSouth(node);
+
+            if (node.danger)
+                return "";
+
+            System.out.println("SI FUNCIONA ==> SOUTH-1");
+            if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+                isExit = true;
+                Node.resetNodesCount();
+                return "S";
+            } else {
+                return "S,";
+            }
+        }
+        return "";
+    }
+
+    private String moveToNorth(Maze maze) {
+        if (node.north) {
+            node = maze.moveNorth(node);
+
+            if (node.danger)
+                return "";
+
+            if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+                isExit = true;
+                Node.resetNodesCount();
+                return "N";
+            } else {
+                return "N,";
+            }
+        }
+        return "";
+    }
+
+    private String moveToUp(Maze maze) {
+        if (node.up) {
+            node = maze.moveUp(node);
+
+            if (node.danger)
+                return "";
+
+            if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+                isExit = true;
+                Node.resetNodesCount();
+                return "U";
+            } else {
+                return "U,";
+            }
+        }
+        return "";
+    }
+
+    private String moveToWest(Maze maze) {
+        if (node.west) {
+            node = maze.moveWest(node);
+
+            if (node.danger)
+                return "";
+
+            if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+                isExit = true;
+                Node.resetNodesCount();
+                return "W";
+            } else {
+                return "W,";
+            }
+        }
+        return "";
+    }
+
+    private String moveToDown(Maze maze) {
+        if (node.down) {
+            node = maze.moveDown(node);
+
+            if (node.danger)
+                return "";
+
+            if (maze.isExitSpace(node.xIndex, node.yIndex, node.zIndex)) {
+                isExit = true;
+                Node.resetNodesCount();
+                return "D";
+            } else {
+                return "D,";
+            }
+        }
+        return "";
     }
 
 }
