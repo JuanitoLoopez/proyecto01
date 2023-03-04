be1966724c5e2180281b27c7c2811f97f95ec8f6
JuanitoLopez <66218285+JuanitoLoopez@users.noreply.github.com>
2023-03-04 02:07:19 -0600

    Update Solver.java

--- archivos_base/Solver.java
+++ archivos_base/Solver.java
@@ -1,14 +1,9 @@
 import java.util.ArrayList;
 
-/*
-    Esta es su clase principal. El unico metodo que debe implementar es
-    public String[] solve(Maze maze)
-    pero es libre de crear otros metodos y clases en este u otro archivo que desee.
-*/
-
 public class Solver {
     
     private ArrayList<String> route;
+    private ArrayList<String> shortestRoute;
     private boolean exit = false;
 
     public Solver() {
@@ -18,13 +13,25 @@ public class Solver {
     public String solve(Maze maze) {
         route.clear();
         exit = false;
+        
         Node startNode = maze.getStartingSpace();
 
-        solveRecursive(maze, startNode, exit);
-        return route.toString().replace(" ", "");
+        solveRecursive(maze, startNode, exit, null);
+        shortestRoute = shortRoute(maze, route);
+
+        return shortestRoute.toString().replace(" ", "");
     }
 
-    private boolean solveRecursive(Maze maze, Node currentNode, boolean exit) {
+    private ArrayList<String> shortRoute(Maze maze, ArrayList<String> route) {
+        //TODO make implementation
+        
+        Node finalNode = maze.getStartingSpace();
+        route.remove(finalNode);
+
+        return route;
+    }
+
+    private boolean solveRecursive(Maze maze, Node currentNode, boolean exit, Node lastNode) {
 
         System.out.print("Result: " + route.toString());
         System.out.println(" Node: x="+currentNode.xIndex+" y="+currentNode.yIndex+" z="+currentNode.zIndex );
@@ -57,38 +64,39 @@ public class Solver {
         
         // Realizamos recursividad
         if(!exit) {
-            if (currentNode.north && !currentNode.danger && !maze.moveNorth(currentNode).isExit) {
+            if (currentNode.north && !currentNode.danger && !maze.moveNorth(currentNode).isExit && lastNode != maze.moveNorth(currentNode) ) {
                 route.add("N");
-                exit = solveRecursive( maze, maze.moveNorth(currentNode), exit );
+                exit = solveRecursive( maze, maze.moveNorth(currentNode), exit, currentNode );
                 if(exit) return true;
             }
-            if (currentNode.east && !currentNode.danger && !maze.moveEast(currentNode).isExit) {
+            if (currentNode.east && !currentNode.danger && !maze.moveEast(currentNode).isExit && lastNode != maze.moveEast(currentNode) ) {
                 route.add("E");
-                exit = solveRecursive( maze, maze.moveEast(currentNode), exit );
+                exit = solveRecursive( maze, maze.moveEast(currentNode), exit, currentNode );
                 if(exit) return true;
             }
-            if (!exit && currentNode.south && !currentNode.danger && !maze.moveSouth(currentNode).isExit) {
+            if (currentNode.south && !currentNode.danger && !maze.moveSouth(currentNode).isExit && lastNode != maze.moveSouth(currentNode) ) {
                 route.add("S");
-                exit = solveRecursive( maze, maze.moveSouth(currentNode), exit );
+                exit = solveRecursive( maze, maze.moveSouth(currentNode), exit, currentNode );
                 if(exit) return true;
             }
-            if (currentNode.west && !currentNode.danger && !maze.moveWest(currentNode).isExit) {
+            if (currentNode.west && !currentNode.danger && !maze.moveWest(currentNode).isExit && lastNode != maze.moveWest(currentNode) ) {
                 route.add("W");
-                exit = solveRecursive( maze, maze.moveWest(currentNode), exit );
+                exit = solveRecursive( maze, maze.moveWest(currentNode), exit, currentNode );
                 if(exit) return true;
             }
-            if (currentNode.up && !currentNode.danger && !maze.moveUp(currentNode).isExit) {
+            if (currentNode.up && !currentNode.danger && !maze.moveUp(currentNode).isExit && lastNode != maze.moveUp(currentNode) ) {
                 route.add("U");
-                exit = solveRecursive( maze, maze.moveUp(currentNode), exit );
+                exit = solveRecursive( maze, maze.moveUp(currentNode), exit, currentNode );
                 if(exit) return true;
             }
-            if (currentNode.down && !currentNode.danger && !maze.moveDown(currentNode).isExit) {
+            if (currentNode.down && !currentNode.danger && !maze.moveDown(currentNode).isExit && lastNode != maze.moveDown(currentNode) ) {
                 route.add("D");
-                exit = solveRecursive( maze, maze.moveDown(currentNode), exit );
+                exit = solveRecursive( maze, maze.moveDown(currentNode), exit, currentNode );
                 if(exit) return true;
             }
         }
         
         return false;
     }
+
 }
